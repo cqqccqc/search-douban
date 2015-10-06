aaa9e3b43fa363a308f4730aa2752c184c96af7d
陈琦 <cqqccqc@foxmail.com>
2015-10-06 22:04:05 +0800

    init tabbar

--- SearchDouban/index.ios.js
+++ SearchDouban/index.ios.js
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App
+ * Douban Search React Native App
  * https://github.com/facebook/react-native
  */
 'use strict';
@@ -8,46 +8,92 @@ var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
+  TabBarIOS,
   Text,
   View,
 } = React;
 
 var SearchDouban = React.createClass({
-  render: function() {
+  
+  getInitialState: function() {
+    return {
+      selectedTab: 'redTab',
+      notifCount: 0,
+      presses: 0,
+    };
+  },
+
+  _renderContent: function(color: string, pageText: string, num?: number) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={[styles.tabContent, {backgroundColor: color}]}>
+        <Text style={styles.tabText}>{pageText}</Text>
+        <Text style={styles.tabText}>{num} re-renders of the {pageText}</Text>
       </View>
     );
-  }
+  },
+
+  renderContent: function() {
+    
+  },
+
+  render: function() {
+    return (
+      <TabBarIOS
+        tintColor="white"
+        barTintColor="darkslateblue">
+        <TabBarIOS.Item
+          title="Blue Tab"
+          systemIcon="bookmarks"
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}>
+          {this._renderContent('#414A8C', 'Blue Tab')}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          systemIcon="history"
+          badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+              notifCount: this.state.notifCount + 1,
+            });
+          }}>
+          {this._renderContent('#783E33', 'Red Tab', this.state.notifCount)}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          //icon={require('./flux.png')}
+          title="More"
+          selected={this.state.selectedTab === 'greenTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'greenTab',
+              presses: this.state.presses + 1
+            });
+          }}>
+          {this._renderContent('#21551C', 'Green Tab', this.state.presses)}
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  },
+
 });
 
 var styles = StyleSheet.create({
-  container: {
+  tabContent: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  tabText: {
+    color: 'white',
+    margin: 50,
   },
 });
+
+
+
 
 AppRegistry.registerComponent('SearchDouban', () => SearchDouban);
