2830deac056d223b8a4ed49b837bb9b9998d0507
q.chen <q.chen@Ctrip.com>
2015-11-16 14:39:16 +0800

    delete

---
+++ SearchDoubanWeb/public/assets/index.js
@@ -0,0 +1,16 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+	var _jquery = __webpack_require__(1);
+
+	var _jquery2 = _interopRequireDefault(_jquery);
+
+	console.log(_jquery2['default']);
+
+/***/ }
+]);
